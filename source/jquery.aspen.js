a2cad03eb692105ba92a42ac67a43e03d2b1e211
Bob Yexley <bob@yexley.net>
2013-05-07 23:34:18 -0400

    Shelled out plugin structure from jqueryboilerplate.com

--- source/jquery.aspen.js
+++ source/jquery.aspen.js
@@ -1,5 +1,36 @@
-(function ($) {
+// The structure/shell of this plugin is based on the shell provided at http://jqueryboilerplate.com
 
+(function ($, window, document, undefined) {
 
+	var pluginName = "aspen";
+	var defaults = {
+		above: undefined,
+		below: undefined,
+		margin: "10"
+	};
 
-}(jQuery));
+	function Plugin (el, options) {
+		this.el = el;
+		this.options = $.extend({}, defaults, options);
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.init();
+	}
+
+	Plugin.prototype = {
+
+		init: function () {
+
+		}
+
+	};
+
+	$.fn[pluginName] = function (options) {
+		return this.each(function () {
+			if (!$.data(this, "plugin_" + pluginName)) {
+				$.data(this, "plugin_" + pluginName, new Plugin(this, options));
+			}
+		});
+	};
+
+}(jQuery, window, document));
